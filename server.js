01ae831e1cea53ee1855381d0a627ce1ce5718e7
gberk <gberkowitz@logmein.com>
2018-01-18 12:04:36 -0500

    Basic google endpoint

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ var mqttClient = require('./mqtt.js');
 app.set('port', (process.env.PORT || 5000));
 server.listen(app.get('port'));
 
+
+app.post('/gAssistant', function(req,res){
+    console.log("here");
+    console.log(req)
+})
+
 app.get('/deviceControl',function(req,res){
     var powerState = req.query["power"];
     if(powerState && (powerState =="on" || powerState=="On" || powerState == "ON")){
